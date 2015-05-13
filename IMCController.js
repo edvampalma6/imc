378ce00bac1da965b207ede0c310c93c193f0888
Edvam <ed.psilveira6@gmail.com>
2015-05-12 21:58:52 -0300

    5

--- IMCController.js
+++ IMCController.js
@@ -1,21 +1,38 @@
 var IMCController = {
+	
+	init: function() {
+		IMCController.setForm();
+	},
+	
+	setForm: function() {
+		var form = document.getElementById('frm-imc');
+		form.addEventListener('submit', function(event) {
+			IMCController.calculateIMC();
+			event.preventDefault(); //to avoid form submition
+		});
+	},
+	
+	calculateIMC: function() {
+		var heightInput = document.getElementById('height');
+		var weightInput = document.getElementById('weight');
+		
+		var height = parseFloat(heightInput.value);
+		var weight = parseFloat(weightInput.value);
+		
+		var result = 0;
+		
+		if(height && weight) {
+			result = IMCService.calculate(height, weight);
+			IMCController.showResult(result);
+		}
+	},
+	
+	showResult: function(result) {
+		var span = document.getElementById('result');
+		span.innerHTML = result.toFixed(2);
+	}
 
-    init: function() {
-        IMCController.setForm();
-};
-    setForm: function() {
-    var form = document.getElementById('frm-imc');
-    form.addEventListener('submit', function(event) {
-    alert('Voce clicou no botão que eu sei!');
-    event.preventDefault(); //to avoid form submition 
-});
-    
-calculateIMC: function() {
-    
 };
 
-showResult: function() {
-    
-};
-
-};
+//initialization
+IMCController.init();
